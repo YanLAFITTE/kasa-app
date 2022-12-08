f176d6cc03793c47a4b445d3e05d234e1c9edb43
yala0 <welcome.yanlafitte@gmail.com>
2022-12-08 05:20:58 +0100

    add css

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -13,7 +13,7 @@ const ErrorPage = () => {
             <main className='error-container'>
                 <h1 className='error-title'>404</h1>
                 <p className='error-paragraph'>
-                    Oups! La page que vous demandez n'existe pas.
+                    <span>Oups! La page que</span> <span>vous demandez n'existe pas.</span>
                 </p>
                 <Link to='/' className='error-link'>
                     Retourner sur la page d'accueil
